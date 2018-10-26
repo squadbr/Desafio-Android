efc5b81aee22497b37c1effc34498be39d345af3
Gabriel Feo <gabriel@gabrielfeo.com>
2018-10-26 11:07:48 -0300

    Moved view module classes to "view" package

--- view/src/main/kotlin/com/gabrielfeo/openmoviedbsearch/view/MainActivity.kt
+++ view/src/main/kotlin/com/gabrielfeo/openmoviedbsearch/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.gabrielfeo.openmoviedbsearch
+package com.gabrielfeo.openmoviedbsearch.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.gabrielfeo.openmoviedbsearch.R
 
 class MainActivity : AppCompatActivity() {
 
