61336c39d5ee214b824efeb4ec2b5eed698092fe
Gabriel Feo <gabriel@gabrielfeo.com>
2018-10-25 23:33:28 -0300

    Changed Gradle build scripts to Gradle Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+import com.android.build.gradle.ProguardFiles.getDefaultProguardFile
+import com.android.build.gradle.internal.dsl.BuildType
+import org.gradle.internal.impldep.com.amazonaws.PredefinedClientConfigurations.defaultConfig
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "com.gabrielfeo.openmoviedbsearch"
+        minSdkVersion(17)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.2.71")
+    implementation("com.android.support:appcompat-v7:28.0.0")
+    implementation("com.android.support.constraint:constraint-layout:1.1.3")
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("com.android.support.test:runner:1.0.2")
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+}
