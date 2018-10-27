dc72bec337a1762d7820038a8dd8608e40534987
Gabriel Feo <gabriel@gabrielfeo.com>
2018-10-26 22:30:27 -0300

    Created basic Movie model
    Created a basic model with plain strings to be used in the app
    initially. Only the used info is read from the API JSON response.

---
+++ model/src/main/java/com/gabrielfeo/openmoviedbsearch/model/Movie.kt
@@ -0,0 +1,20 @@
+package com.gabrielfeo.openmoviedbsearch.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    @SerializedName("Title") val title: String,
+    @SerializedName("imdbId") val id: String,
+    @SerializedName("imdbRating") val rating: String,
+    @SerializedName("Director") val director: String,
+    @SerializedName("Plot") val synopsis: String,
+    @SerializedName("Language") val languages: String,
+    @SerializedName("Country") val countries: String,
+    @SerializedName("Year") val year: String,
+    @SerializedName("Runtime") val runtime: String,
+    @SerializedName("Poster") val posterUrl: String
+) {
+
+    override fun equals(other: Any?): Boolean = (other as? Movie)?.id == this.id
+
+}
