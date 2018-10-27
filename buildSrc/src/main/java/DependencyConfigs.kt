e99b1c56839f8e84cea15a79d12760fdfd7a6e9d
Gabriel Feo <gabriel@gabrielfeo.com>
2018-10-26 21:33:01 -0300

    Declared inter-module dependencies

--- buildSrc/src/main/java/DependencyConfigs.kt
+++ buildSrc/src/main/java/DependencyConfigs.kt
@@ -1,3 +1,4 @@
+import org.gradle.kotlin.dsl.project
 
 internal val sharedDependencies: DependencyConfigFunction = {
     "implementation"("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.2.71")
@@ -9,10 +10,12 @@ internal val modelModuleDependencies: DependencyConfigFunction = {
 }
 
 internal val dataModuleDependencies: DependencyConfigFunction = {
-
+    "implementation"(project(":model"))
 }
 
 internal val viewModuleDependencies: DependencyConfigFunction = {
+    "implementation"(project(":model"))
+    "implementation"(project(":data"))
     "implementation"("com.android.support:appcompat-v7:28.0.0")
     "implementation"("com.android.support.constraint:constraint-layout:1.1.3")
     "testImplementation"("junit:junit:4.12")
